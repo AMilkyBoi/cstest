c4d963a125b261c256554eec492cb3238d791032
Nathan Phillips <30729007+AMilkyBoi@users.noreply.github.com>
2021-02-09 14:32:14 -0600

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,17 @@
+/* Tells the program to use the System namespace */
+using System;
+
+/* The namespace declaration indicates a collection of classes */
+namespace HelloWorld
+{
+    /* Classes can contain multiple methods, which define the behavior of the class */
+    class Program
+    {
+        /* Defines the 'Main' method, which states what the class does when it is executed */
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine writes a line to the console, which can then be read later */
+            Console.WriteLine("Hello World");
+        }
+    } 
+}
